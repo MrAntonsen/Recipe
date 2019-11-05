26ef0657eb5e75944824e03274e366414cb03bdd
MrHaika <morganra92@hotmail.com>
2019-11-05 16:12:13 +0100

    second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyAoUxMfjX_ULYnzRcxMERA0_gm99lFO-h0'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyAoUxMfjX_ULYnzRcxMERA0_gm99lFO-h0'
 };
 
 /*
